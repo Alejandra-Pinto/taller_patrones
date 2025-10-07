c187cfe4627713564761f9114b813f7458893e52
Alejandra Pinto <alejandrapinto@unicauca.edu.co>
2025-10-06 20:00:44 -0500

    vrs division por paquetes

---
+++ src/main/java/co/edu/unicauca/desingpatterns/TallerPatrones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.unicauca.desingpatterns;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class TallerPatrones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
