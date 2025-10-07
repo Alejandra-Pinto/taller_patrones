ae810431a1b16c04c4d5eaafed16a2e000fd27e6
Juliana <mariajulisanchez@unicauca.edu.co>
2025-10-06 22:14:10 -0500

    Agregué instancias y aplicación del patrón Decorator en el main

--- src/main/java/co/edu/unicauca/desingpatterns/TallerPatrones.java
+++ src/main/java/co/edu/unicauca/desingpatterns/TallerPatrones.java
@@ -1,9 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package co.edu.unicauca.desingpatterns;
 
+import co.edu.unicauca.DesignPatterns.decorator.PriorityProject;
+import co.edu.unicauca.DesignPatterns.decorator.ProjectD;
 import co.edu.unicauca.DesignPatterns.facade.PlatformFacade;
 import co.edu.unicauca.DesignPatterns.facade.ProjectFacade;
 import java.util.Arrays;
@@ -15,6 +13,7 @@ import java.util.Arrays;
 public class TallerPatrones {
 
     public static void main(String[] args) {
+        //INSTANCIAS DE PATRÓN FACADE
         ProjectFacade investigacion = new ProjectFacade(
                 "Modelo IA para deteccion de enfermedades",
                 "Investigacion",
@@ -37,9 +36,24 @@ public class TallerPatrones {
                 "Implementar un sistema web eficiente.",
                 Arrays.asList("Desarrollo de base de datos", "Implementar CRUD", "Desplegar aplicacion")
         );
+        
+        //INSTANCIAS DE PATRÓN DECORATE
+        ProjectD normalProject = new ProjectD("Plataforma de aprendizaje adaptativo basada en IA", 
+                "Diseño e implementación de un sistema inteligente que personaliza el contenido educativo según el desempeño del estudiante");
+        
+        ProjectD priorityProject = new PriorityProject(normalProject);
 
+        
+        //APLICANDO PATRÓN FACADE
         PlatformFacade plataforma = new PlatformFacade();
         plataforma.manageProject(investigacion);
         plataforma.manageProject(practica);
+        
+        
+        //APLICANDO PATRÓN DECORATOR
+        System.out.println("PATRÓN DECORATOR");
+        System.out.println("PROYECTO: " + normalProject.getDescription());
+        System.out.println("PROYECTO CON PATRÓN DECORADOR: " + priorityProject.getDescription());
+        
     }
 }
